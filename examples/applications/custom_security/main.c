a033fc87d9fa1391a4d044ed4a52e67631baaadc
Ira Stevens <ira.stevens@verizon.com>
2018-01-17 15:14:39 -0500

    moved platforms added mbedtls config

---
+++ examples/applications/custom_security/main.c
@@ -0,0 +1,7 @@
+// Copyright (C) 2017, 2018 Verizon, Inc. All rights reserved.
+
+/*
+ * Demonstrate how the user can build and plugin their own security (e.g., a MBEDTLS variant).
+ */
+
+int main() {}
