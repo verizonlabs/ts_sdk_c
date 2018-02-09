59647df779edfd7729333201e426c244968510ca
Nilangshu <nilangshu.bidyanta@one.verizon.com>
2018-02-09 18:32:51 -0500

    Changed the build target to the Nucleo-L476 board in the top-level CMakeLists.txt; Prevent the "test_driver" test from being built (temporarily) since it only works for OS-based systems; Added an additional test to verify the debug interface's functionality; Added a linker script since the automatically generated linker script is faulty;

--- scripts/cmake.bash
+++ scripts/cmake.bash
@@ -1,9 +1,17 @@
 #!/bin/bash
-cmake . -B./cmake-build-debug \
-	-DCMAKE_BUILD_TYPE=Debug \
-	-DTOOLCHAIN_PREFIX=/Users/v585584/Source/embedded/toolchain/gcc-arm-none-eabi-7-2017-q4-major \
-	-DTARGET_TRIPLET=arm-none-eabi \
-	-DSTM32Cube_DIR=/Users/v585584/Source/embedded/STM32Cube_FW_F4_V1.19.0 \
-	-DSTM32_CHIP=STM32F401RE \
-	-DCMAKE_TOOLCHAIN_FILE=/Users/v585584/Source/embedded/stm32-cmake/cmake/gcc_stm32.cmake 
+#cmake . -B./cmake-build-debug \
+#	-DCMAKE_BUILD_TYPE=Debug \
+#	-DTOOLCHAIN_PREFIX=/Users/Ira/source/embedded/toolchain/gcc-arm-none-eabi-7-2017-q4-major \
+#	-DTARGET_TRIPLET=arm-none-eabi \
+#	-DSTM32Cube_DIR=/Users/Ira/source/embedded/STM32Cube_FW_F4_V1.19.0 \
+#	-DSTM32_CHIP=STM32F401RE \
+#	-DCMAKE_TOOLCHAIN_FILE=/Users/Ira/source/embedded/stm32-cmake/cmake/gcc_stm32.cmake
 
+cmake . -B./cmake-build-debug \
+	-DBUILD_EXAMPLES=ON \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DTOOLCHAIN_PREFIX=/Users/v768213/work/cortexm/gcc-arm-none-eabi-7-2017-q4-major \
+	-DTARGET_TRIPLET=arm-none-eabi \
+	-DSTM32Cube_DIR=/Users/v768213/work/cortexm/STM32Cube_FW_L4_V1.11.0 \
+	-DSTM32_CHIP=STM32L476RG \
+	-DCMAKE_TOOLCHAIN_FILE=/Users/v768213/work/stm32-cmake/cmake/gcc_stm32.cmake
