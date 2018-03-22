1428dbfaf6d2fd1f01e966775bf420d5a8fd0de3
Ira Stevens <ira.stevens@verizon.com>
2018-03-22 15:03:04 -0400

    update firewall tests

--- examples/tests/test_firewall.c
+++ examples/tests/test_firewall.c
@@ -5,7 +5,7 @@
 int main() {
 
 	TsStatus_t status;
-	ts_status_set_level( TsStatusLevelDebug );
+	ts_status_set_level( TsStatusLevelTrace );
 
 	// create new firewall
 	TsFirewallRef_t firewall;
@@ -13,7 +13,7 @@ int main() {
 
 	// test simple configuration setting
 	char * xmessage =
-		"{\"transactionid\":\"00000000-0000-0000-0000-000000000000\","
+		"{\"transactionid\":\"00000000-0000-0000-0000-000000000001\","
 		"\"kind\":\"ts.event.firewall\","
 		"\"action\":\"set\","
 		"\"fields\":{"
@@ -23,7 +23,15 @@ int main() {
 			"\"default_rules\":[{\"sense\":\"outbound\",\"action\":\"drop\",\"destination\":{\"address\":\"35.194.94.155\"}},"
 				"{\"sense\":\"outbound\",\"action\":\"drop\",\"destination\":{\"address\":\"35.194.94.156\"}}]"
 		"}}}";
-
+	char * ymessage =
+		"{\"transactionid\":\"00000000-0000-0000-0000-000000000002\","
+			"\"kind\":\"ts.event.firewall\","
+			"\"action\":\"get\","
+			"\"fields\":{"
+			"\"configuration\":null,"
+			"\"rules\":null"
+			"}}";
+/*
 	// test decoding to message from json
 	TsMessageRef_t from_json;
 	ts_status_debug( "test_firewall: create message from_json, %s\n", ts_status_string( ts_message_create( &from_json ) ) );
@@ -46,14 +54,19 @@ int main() {
 	ts_status_debug( "test_firewall: decode ts-cbor, %s\n", ts_status_string( ts_message_decode( from_cbor, TsEncoderTsCbor, buffer, buffer_size ) ) );
 	ts_status_debug( "test_firewall: encode debug, %s\n", ts_status_string( ts_message_encode( from_cbor, TsEncoderDebug, NULL, 0 ) ) );
 	ts_status_debug( "test_firewall: destroy from_cbor, %s\n", ts_status_string( ts_message_destroy( from_cbor ) ) );
-
-	// test firewall handler
+*/
+	// test firewall handler, set
 	TsMessageRef_t message;
 	ts_status_debug( "test_firewall: create firewall message, %s\n", ts_status_string( ts_message_create( &message ) ) );
 	ts_status_debug( "test_firewall: decode json, %s\n", ts_status_string( ts_message_decode( message, TsEncoderJson, (uint8_t*)xmessage, strlen(xmessage) ) ) );
+	ts_status_debug( "test_firewall: handle set firewall message, %s\n", ts_status_string( ts_firewall_handle( firewall, message ) ) );
+	ts_status_debug( "test_firewall: destroy firewall message, %s\n", ts_status_string( ts_message_destroy( message ) ) );
 
-	ts_status_debug( "test_firewall: handle firewall message, %s\n", ts_status_string( ts_firewall_handle( firewall, message ) ) );
-
+	// test firewall handler, get
+	ts_status_debug( "test_firewall: create firewall message, %s\n", ts_status_string( ts_message_create( &message ) ) );
+	ts_status_debug( "test_firewall: decode json, %s\n", ts_status_string( ts_message_decode( message, TsEncoderJson, (uint8_t*)ymessage, strlen(ymessage) ) ) );
+	ts_status_debug( "test_firewall: handle get firewall message, %s\n", ts_status_string( ts_firewall_handle( firewall, message ) ) );
+	ts_status_debug( "test_firewall: result, %s\n", ts_status_string( ts_message_encode( message, TsEncoderDebug, NULL, 0 ) ) );
 	ts_status_debug( "test_firewall: destroy firewall message, %s\n", ts_status_string( ts_message_destroy( message ) ) );
 
 
