5a41d37903d11d3890b6e2c9dcc7bd1f9afc5d24
McIrvin, Matthew J <matthew.mcirvin@one.verizon.com>
2018-05-25 17:47:05 -0400

    setting and getting parameters

--- sdk/source/ts_log.c
+++ sdk/source/ts_log.c
@@ -40,6 +40,56 @@ TsStatus_t ts_logconfig_destroy(TsLogConfigRef_t logconfig) {
 	return TsStatusOk;
 }
 
+static TsStatus_t _ts_handle_set( TsLogConfigRef_t logconfig, TsMessageRef_t fields ) {
+	if (ts_message_get_bool(fields, "enabled", &(logconfig->_enabled))
+			== TsStatusOk) {
+		ts_status_debug("_ts_handle_set: enabled = %d\n", logconfig->_enabled);
+	}
+	if (ts_message_get_int(fields, "level", &(logconfig->_level))
+			== TsStatusOk) {
+		ts_status_debug("_ts_handle_set: level = %d\n", logconfig->_level);
+	}
+	if (ts_message_get_int(fields, "max_entries", &(logconfig->_max_entries))
+			== TsStatusOk) {
+		ts_status_debug("_ts_handle_set: max_entries = %d\n", logconfig->_max_entries);
+	}
+	if (ts_message_get_int(fields, "min_interval", &(logconfig->_min_interval))
+			== TsStatusOk) {
+		ts_status_debug("_ts_handle_set: min_interval = %d\n", logconfig->_min_interval);
+	}
+	if (ts_message_get_int(fields, "reporting_interval", &(logconfig->_reporting_interval))
+			== TsStatusOk) {
+		ts_status_debug("_ts_handle_set: reporting_interval = %d\n", logconfig->_reporting_interval);
+	}
+	return TsStatusOk;
+}
+
+static TsStatus_t _ts_handle_get( TsLogConfigRef_t logconfig, TsMessageRef_t fields ) {
+	TsMessageRef_t contents;
+	if (ts_message_has(fields, "enabled", &contents) == TsStatusOk) {
+		ts_status_debug("_ts_handle_get: get enabled\n");
+		ts_message_set_bool(fields, "enabled", logconfig->_enabled);
+	}
+	if (ts_message_has(fields, "level", &contents) == TsStatusOk) {
+		ts_status_debug("_ts_handle_get: get level\n");
+		ts_message_set_int(fields, "level", logconfig->_level);
+	}
+	if (ts_message_has(fields, "max_entries", &contents) == TsStatusOk) {
+		ts_status_debug("_ts_handle_get: get max_entries\n");
+		ts_message_set_int(fields, "max_entries", logconfig->_max_entries);
+	}
+	if (ts_message_has(fields, "min_interval", &contents) == TsStatusOk) {
+		ts_status_debug("_ts_handle_get: get min_interval\n");
+		ts_message_set_int(fields, "min_interval", logconfig->_min_interval);
+	}
+	if (ts_message_has(fields, "reporting_interval", &contents) == TsStatusOk) {
+		ts_status_debug("_ts_handle_get: get reporting_interval\n");
+		ts_message_set_int(fields, "reporting_interval", logconfig->_reporting_interval);
+	}
+	return TsStatusOk;
+}
+
+
 /**
  * Handle a log configuration message.
  * @param logconfig
@@ -55,7 +105,57 @@ TsStatus_t ts_logconfig_handle(TsLogConfigRef_t logconfig, TsMessageRef_t messag
 	ts_status_trace("ts_logconfig_handle");
 	ts_platform_assert(logconfig != NULL);
 	ts_platform_assert(message != NULL);
-	return TsStatusOk;
+
+	TsStatus_t status;
+
+	char * kind;
+	status = ts_message_get_string(message, "kind", &kind);
+	if ((status == TsStatusOk) && (strcmp(kind, "ts.event.logconfig") == 0)) {
+
+		char * action;
+		status = ts_message_get_string(message, "action", &action);
+		if (status == TsStatusOk) {
+
+			TsMessageRef_t fields;
+			status = ts_message_get_message(message, "fields", &fields);
+			if (status == TsStatusOk) {
+
+				if (strcmp(action, "set") == 0) {
+
+					// set or update a rule or domain
+					ts_status_debug(
+							"ts_logconfig_handle: delegate to set handler\n");
+					status = _ts_handle_set(logconfig, fields);
+
+				} else if (strcmp(action, "get") == 0) {
+
+					// get a rule or list of rules
+					ts_status_debug(
+							"ts_logconfig_handle: delegate to get handler\n");
+					status = _ts_handle_get(logconfig, fields);
+
+				} else {
+
+					ts_status_info(
+							"ts_logconfig_handle: message missing valid action.\n");
+					status = TsStatusErrorBadRequest;
+				}
+			} else {
+
+				ts_status_info("ts_logconfig_handle: message missing fields.\n");
+				status = TsStatusErrorBadRequest;
+			}
+		} else {
+
+			ts_status_info("ts_logconfig_handle: message missing action.\n");
+			status = TsStatusErrorBadRequest;
+		}
+	} else {
+
+		ts_status_info("ts_logconfig_handle: message missing correct kind.\n");
+		status = TsStatusErrorBadRequest;
+	}
+	return status;
 }
 
 /**
