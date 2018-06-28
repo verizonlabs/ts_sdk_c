13cf7989e49f18eb480eb5fbb27d960df04e8e6b
McIrvin, Matthew J <matthew.mcirvin@one.verizon.com>
2018-06-28 15:44:35 -0400

    logging of logging suspensions

--- sdk/source/ts_suspend.c
+++ sdk/source/ts_suspend.c
@@ -2,12 +2,30 @@
 
 #include "ts_suspend.h"
 #include "ts_platform.h"
+#include "ts_log.h"
 
 static TsStatus_t _ts_handle_get( TsMessageRef_t fields );
 static TsStatus_t _ts_handle_set( TsMessageRef_t fields );
 
 static bool _suspend_firewall = false;
 static bool _suspend_logconfig = false;
+static TsLogConfigRef_t logconfig = NULL;
+
+#define SUSPEND_LOG(s) if (logconfig != NULL) { ts_log(logconfig, TsLogLevelInfo, TsCategoryDiagnostic, s); }
+
+/**
+ * Set the logconfig used to log suspension events.
+ * @param logconfig
+ * [in] The log configuration to use for logging suspension events.
+ * @return
+ * The return status (TsStatus_t) of the function, see ts_status.h for more information.
+ * - TsStatusOk
+ * - TsStatusError[Code]
+ */
+TsStatus_t ts_suspend_set_logconfig(TsLogConfigRef_t config) {
+	logconfig = config;
+	return TsStatusOk;
+}
 
 /**
  * Handle a suspension message.
@@ -97,12 +115,19 @@ static TsStatus_t _ts_handle_set( TsMessageRef_t fields ) {
 			ts_status_debug("_ts_handle_set: resuming firewall\n");
 		}
 	}
-	if (ts_message_get_bool(fields, "logconfig", &_suspend_logconfig) == TsStatusOk) {
-		if (_suspend_logconfig) {
+	bool logsuspend;
+	if (ts_message_get_bool(fields, "logconfig", &logsuspend) == TsStatusOk) {
+		if (logsuspend) {
 			ts_status_debug("_ts_handle_set: suspending logging\n");
+			// log before suspending the logging
+			SUSPEND_LOG("Logging suspended\n");
+			_suspend_logconfig = logsuspend;
 		}
 		else {
 			ts_status_debug("_ts_handle_set: resuming logging\n");
+			// log after resuming the logging
+			_suspend_logconfig = logsuspend;
+			SUSPEND_LOG("Logging resumed\n");
 		}
 	}
 	return TsStatusOk;
