061af558a485a96bf56dbb329097b89791ee9e73
McIrvin, Matthew J <matthew.mcirvin@one.verizon.com>
2018-09-07 13:35:52 -0400

    disable excess spew

--- sdk/source/ts_log.c
+++ sdk/source/ts_log.c
@@ -278,24 +278,24 @@ TsStatus_t ts_log(TsLogConfigRef_t log, TsLogLevel_t level, TsLogCategory_t cate
 		if ((time - previous > 0) && (time - previous < log->_min_interval)) {
 			if (log->_newest->level == level && log->_newest->category == category) {
 				if (strncmp(log->_newest->body, text, LOG_MESSAGE_MAX_LENGTH) == 0) {
-					ts_status_debug("ts_log: Log messages identical within interval %d\n", log->_min_interval);
+					//ts_status_debug("ts_log: Log messages identical within interval %d\n", log->_min_interval);
 					log->_log_in_progress = false;
 					return TsStatusOk;
 				}
 				else {
-					ts_status_debug("ts_log: Log messages NOT identical within interval %d\n", text);
+					//ts_status_debug("ts_log: Log messages NOT identical within interval %d\n", text);
 				}
 			}
 		}
 		else {
-			ts_status_debug("enough time elapsed: %d\n", time-previous);
+			//ts_status_debug("enough time elapsed: %d\n", time-previous);
 		}
 	}
 	else {
-		ts_status_debug("no preexisting messages\n");
+		//ts_status_debug("no preexisting messages\n");
 	}
 
-	ts_status_debug("ts_log: logging message %s\n", text);
+	//ts_status_debug("ts_log: logging message %s\n", text);
 
 	// Can we get the space for the message body?
 	// note, strnlen excludes the terminating null; we add it back in
